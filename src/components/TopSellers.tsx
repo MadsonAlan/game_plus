98af64628fb8b018fafc9b1643cf91a348b1cdc7
MadsonAlan <madson99hitech@gmail.com>
2021-10-04 22:00:14 -0300

    atualização do layout

--- src/components/TopSellers.tsx
+++ src/components/TopSellers.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from '../../styles/components/TopSellers.module.css'
 import CardComponent from './Card'
 import defaltCardsData from '../pages/api/lib/gamesWithDiscounts.json'
+import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos } from "react-icons/md";
 
 export default function TopSellers() {
+
+    const [scrollX, setScrollX] = useState(0)
+
+    const handleClick = (direction: string) =>{
+        let x = scrollX + 18 
+        let x2 = scrollX - 18
+        
+        if (direction === "left") {
+            if (scrollX > 0) {
+                setScrollX(x)
+            }
+        }
+        if (direction === "right") {
+            if (scrollX >= 0) {
+                setScrollX(x2)
+            }
+        }
+        console.log(scrollX);
+        
+    }
     return (
         <div className={styles.main}>
             <h3>Os Mais vendidos</h3>
-            <div className={styles.cardCarroussel}>
+            <div className={styles.wrapper}>
+            <MdOutlineArrowBackIosNew className={styles.sliderArrowBack} onClick={()=>handleClick("left")}/>
+            <div className={styles.cardCarroussel} style={{  transform: `translateX(${scrollX}rem)` }}>
                 {
                     defaltCardsData.map((cardData, indice) => {
                         return (
@@ -19,6 +42,8 @@ export default function TopSellers() {
                     })
                 }
 
+            </div>
+            <MdOutlineArrowForwardIos className={styles.sliderArrowForward} onClick={()=>handleClick("right")}/>
             </div>
         </div>
     )
