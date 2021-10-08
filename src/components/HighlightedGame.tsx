0d2de5f542c866c9923861b81f26168401ddc834
MadsonAlan <madson99hitech@gmail.com>
2021-10-08 07:40:03 -0300

    layout ajustado

--- src/components/HighlightedGame.tsx
+++ src/components/HighlightedGame.tsx
@@ -3,33 +3,42 @@ import { GetStaticProps } from 'next'
 import { PropsCard } from '../types/types'
 import axios from 'axios';
 
-function HighlightedGames({cardInformation}:PropsCard){   
-    
-    return(
-        <section className={styles.featured} style={{backgroundImage: `linear-gradient( to top, rgba(var(--primary-purple), 1), transparent), url('${cardInformation.gameImgURL.replace('capsule_sm_120','header')}')`}}>
+function HighlightedGames({ cardInformation }: PropsCard) {
+
+    return (
+        <section className={styles.featured} style={{ backgroundImage: `linear-gradient( to top, rgba(var(--primary-purple), 1), transparent), url('${cardInformation.gameImgURL.replace('capsule_sm_120', 'header')}')` }}>
             <div className={styles.featuredVertical}>
                 <div className={styles.featuredHorizontal}>
-                <div className={styles.featuredName}>
+                    <div className={styles.featuredName}>
 
-                {cardInformation.gameName}
-                <span>{cardInformation.dataCriacao}</span>
-                </div>
-                <div className={styles.featuredInfo}>
-                    <div className={styles.discountPercentage}>{cardInformation.desconto}</div>
-                    <div>{cardInformation.precAnterior}</div>
-                    <div>{cardInformation.precAtual}</div>
-                    
-                </div>
-                <div>
-                    <a href={cardInformation.urlGameSteam}>
+                        {cardInformation.gameName}
+                    </div>
+                    <div className={styles.featuredInfo}>
+                        <div className={styles.featuredLauch}>Lançado em: {cardInformation.dataCriacao}</div>
+                        <div className={styles.discountPercentage}>
+                            <span>
+                                {cardInformation.desconto}
+                            </span>
+                            <div>
+                                <p>
+                                    {cardInformation.precAnterior}
+                                </p>
+                                <span>
+                                    {cardInformation.precAtual}
+                                </span>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <a className={styles.featuredBuyButton} href={cardInformation.urlGameSteam} target="_blank" rel="noreferrer">
                         comprar
                     </a>
-                </div>
-                <div>
-                    <p>
-                        teste de noticias
-                    </p>
-                </div>
+                    {/* <div>
+                        <p>
+                            teste de noticias
+                        </p>
+                    </div> */}
                 </div>
             </div>
         </section>
